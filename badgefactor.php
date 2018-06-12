617b3bb5d8f0738bcf670bcde3ce9ac86c074bd7
Daniel Le Blanc <daniel.leblanc@ctrlweb.ca>
2018-06-12 15:45:50 -0400

    Update badgefactor.php

--- badgefactor.php
+++ badgefactor.php
@@ -1774,7 +1774,7 @@ class BadgeFactor
     public function badgefactor_scripts()
     {
         wp_register_script('badgefactor-script', plugins_url('/assets/js/bf.js', __FILE__));
-        wp_enqueue_script('badgefactor-script');
+        wp_enqueue_script('badgefactor-script', '', ['jquery']);
         wp_localize_script('badgefactor-script', 'MyAjax', array(
             'url' => admin_url('admin-ajax.php'),
             'nonce' => wp_create_nonce('myajax-nonce'),
